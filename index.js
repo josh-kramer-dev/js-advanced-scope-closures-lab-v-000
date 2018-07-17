918fd35c68d39041bd589535ae1c303c74640c45
github username <github email address>
2018-07-16 23:52:22 -0700

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,29 @@ function produceDrivingRange(blockRange){
     }
   }
 }
+
+
+//produceTipCalculator return a function
+//it configures the percentage tip returned
+
+// let tenPercentTip = produceTipCalculator(.10)
+// expect(tenPercentTip(50)).to.equal(5)
+// expect(tenPercentTip(30)).to.equal(3)
+
+function produceTipCalculator(percentage){
+  return function(fare){
+    return fare * percentage;
+  }
+}
+
+
+function createDriver(name){
+  let driverId = 0
+  return class {
+    constructor(name){
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+
+}
